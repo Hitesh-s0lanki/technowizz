324d22b8be2077bcece94aa9fd68004175ce8ed2
Hitesh Solanki <hiteshsolanki4623@gmail.com>
2023-09-14 17:25:00 +0530

    tag Change

--- src/App.js
+++ src/App.js
@@ -12,9 +12,21 @@ function App() {
   const onTap = () =>{
     setLoginToggle(!LoginToggle)
   }
+  
+  const {getUser, setUser } = useAuthContext()
+  
+  const [toggle, setToggle] = useState(true)
 
-  const {getUser } = useAuthContext()
-
+  document.addEventListener('visibilitychange', function (){
+      setUser("")
+      if(toggle){
+        document.title = "Please don't Change Tab"
+      }else {
+        document.title = "Logical Quiz"
+      }
+      setToggle(!toggle)
+  });
+  
   if ( getUser() === ""){
     return (
       <div className="main bg-orange-200 h-screen">
